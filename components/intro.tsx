379575a6fb3c260a8b5e6423d6e8bf1c343d028e
petertrai <petertrai@gmail.com>
2023-08-30 10:25:11 -0700

    about me

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,26 +1,95 @@
+"use client";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
 import Image from "next/image";
 import React from "react";
 import profilejpg from "../public/profile.jpeg";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
-          <Image
-            src={profilejpg}
-            alt="Ricardo"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover shadow-xl border-[0.35rem] border-white" 
-          />
-          <span className="text-4xl absolute bottom-0 right-0">
-          👋
-          </span>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={profilejpg}
+              alt="Ricardo"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover shadow-xl border-[0.35rem] border-white"
+            />
+          </motion.div>
+
+          <motion.span
+            className="text-3xl absolute bottom-0 right-0 "
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
       </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <span className="font-bold">{`Hello, I'm Peter.`}</span> {`I'm a `}
+        <span className="font-bold">{`full-stack developer`}</span> {`in `}
+        <span className="font-bold">{`Los Angeles. `}</span>
+        {`I enjoy building`}
+        <span className="italic">{` sites & apps. `}</span>
+        {`My focus is `}
+        <span className="underline">React (Next.JS)</span>.
+      </motion.h1>
+      
+      <motion.div className="flex flex-col sm:flex-row justify-center gap-2 px-4 text-lg font-medium"
+      initial={{opacity: 0, y: 100}}
+      animate={{opacity: 1, y: 0}}
+      transition={{
+        delay: 0.1
+      }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:outline-none focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition"/>
+        </Link>
+
+        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/Resume.pdf" download>
+          Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
+        </a>
+
+        <a className="bg-white p-4 text-gray-700 flex items-center rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/peter-arguelles123" target="_blank">
+          <BsLinkedin/>
+        </a>
+
+        <a className="bg-white p-4 text-gray-700 flex items-center rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] active:scale-105 hover:text-gray-950 transition cursor-pointer border border-black/10"
+        href="https://github.com/petertrai" target="_blank"
+        ><FaGithubSquare  />
+        </a>
+      </motion.div>
     </section>
   );
 }
